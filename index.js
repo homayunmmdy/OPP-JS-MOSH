f7dd855149dcd04154069eda16c3333d7db53322
Homayoun <homayoun763@gmail.com>
2024-09-24 23:40:26 +0330

    StopWatch exercise

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-function Circle(radius) {
-  this.radius = radius;
-  let defaultLocation = { x: 0, y: 0 };
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
 
-  this.draw = function () {
-    console.log("draw");
+  this.start = function () {
+    if (running) throw new Error("Stopwatch has already started.");
+
+    running = true;
+
+    startTime = new Date();
   };
 
-  Object.defineProperty(this, "defaultLocation", {
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch is not started.");
+
+    running = false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+  Object.defineProperty(this, "duration", {
     get: function () {
-      return defaultLocation;
-    },
-    set: function (value) {
-      if (!value.x || !value.y) throw new Error("Invalid location");
-      defaultLocation = value;
+      return duration;
     },
   });
 }
 
-const circle = new Circle(2);
-circle.defaultLocation = 1
-circle.draw();
+const sw = new Stopwatch()
