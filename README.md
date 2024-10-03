b81b1c665c9b08e87e8558a48db045adb7182b69
Homayoun <homayoun763@gmail.com>
2024-10-03 21:48:11 +0330

    Method Overriding

--- README.md
+++ README.md
@@ -542,3 +542,29 @@ const s = new Shape();
 const c = new Circle(1, "red");
 
 ```
+
+## Method Overriding
+```javascript
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
+function Shape() {}
+
+Shape.prototype.dublicate = function () {
+  console.log("dublication");
+};
+
+function Circle() {}
+
+extend(Circle, Shape);
+
+Circle.prototype.dublicate = function () {
+  Shape.prototype.dublicate()
+  console.log("dublication Circle");
+};
+
+const c = new Circle();
+
+```
--- index.js
+++ index.js
@@ -1,32 +1,21 @@
-function Shape(color) {
-  this.color = color;
-}
-
-Shape.prototype.dublicate = function () {
-  console.log("dublication");
-};
-
 function extend(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
 
-function Circle(radius, color) {
-  Shape.call(this, color);
-  this.radius = radius;
-}
+function Shape() {}
 
-extend(Circle, Shape)
-Circle.prototype.draw = function () {
-  console.log("draw");
+Shape.prototype.dublicate = function () {
+  console.log("dublication");
 };
 
-function Squre(size) {
-  this.size = size;
-}
+function Circle() {}
 
-extend(Squre, Shape)
+extend(Circle, Shape);
 
+Circle.prototype.dublicate = function () {
+  Shape.prototype.dublicate()
+  console.log("dublication Circle");
+};
 
-const s = new Shape();
-const c = new Circle(1, "red");
+const c = new Circle();
