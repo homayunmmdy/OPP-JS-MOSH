d08bc53c37c8a13db74f2f61db70e0a57d63f936
Homayoun <homayoun763@gmail.com>
2024-10-03 21:38:28 +0330

    Intermediate Function Inheritance

--- README.md
+++ README.md
@@ -504,3 +504,41 @@ Circle.prototype.draw = function () {
 const s = new Shape();
 const c = new Circle(1, "red");
 ```
+
+## Intermediate Function Inheritance
+this extend we called intermedia function inheritance
+```javascript
+function Shape(color) {
+  this.color = color;
+}
+
+Shape.prototype.dublicate = function () {
+  console.log("dublication");
+};
+
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
+}
+
+function Circle(radius, color) {
+  Shape.call(this, color);
+  this.radius = radius;
+}
+
+extend(Circle, Shape)
+Circle.prototype.draw = function () {
+  console.log("draw");
+};
+
+function Squre(size) {
+  this.size = size;
+}
+
+extend(Squre, Shape)
+
+
+const s = new Shape();
+const c = new Circle(1, "red");
+
+```
--- index.js
+++ index.js
@@ -2,21 +2,31 @@ function Shape(color) {
   this.color = color;
 }
 
-Shape.prototype.dublicate = function() {
-  console.log('dublication')
+Shape.prototype.dublicate = function () {
+  console.log("dublication");
+};
+
+function extend(Child, Parent) {
+  Child.prototype = Object.create(Parent.prototype);
+  Child.prototype.constructor = Child;
 }
 
 function Circle(radius, color) {
-  Shape.call(this, color)
-  this.radius = radius
+  Shape.call(this, color);
+  this.radius = radius;
 }
 
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
+extend(Circle, Shape)
+Circle.prototype.draw = function () {
+  console.log("draw");
+};
 
-Circle.prototype.draw = function() {
-  console.log('draw')
+function Squre(size) {
+  this.size = size;
 }
+
+extend(Squre, Shape)
+
 
 const s = new Shape();
-const c = new Circle(1, 'red');
+const c = new Circle(1, "red");
